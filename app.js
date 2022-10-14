c0d4cde0cd69b4af41ffb168057b7dc248a7773b
lsantarcangelo <lsantarcangelo@yahoo.com.ar>
2022-10-14 18:59:53 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+
+//recursos estaticos
+app.use(express.static('public'));
+
+app.listen(PORT, console.log('Servidor corriendo en el puerto ' + PORT));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
