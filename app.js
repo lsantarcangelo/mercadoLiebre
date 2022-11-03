5cf3eeba9cc4a3779b8eb4ba89703e3a350a176b
lsantarcangelo <lsantarcangelo@yahoo.com.ar>
2022-11-02 21:18:11 -0300

    actualizacion de package.json para deploy

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 
 //recursos estaticos
 app.use(express.static('public'));
 
-app.listen(PORT, console.log('Servidor corriendo en el puerto ' + PORT));
+app.listen(port, console.log('Servidor corriendo en el puerto ' + port));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
